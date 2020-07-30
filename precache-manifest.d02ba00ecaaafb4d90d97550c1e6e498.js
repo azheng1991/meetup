e09ba544fbfc0ed861f0b9b129a40f0adf9a87c7
Allan Zheng <azheng34@outlook.com>
2020-07-30 16:01:45 -0400

    Updates

---
+++ precache-manifest.d02ba00ecaaafb4d90d97550c1e6e498.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97753fa98b8547d06d5a3728616bfae7",
+    "url": "/meetup/index.html"
+  },
+  {
+    "revision": "d5ad8c5cbe0b8bcfa085",
+    "url": "/meetup/static/css/main.f4e3a2de.chunk.css"
+  },
+  {
+    "revision": "c1ff76468d4cd14940c9",
+    "url": "/meetup/static/js/2.4005359f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/meetup/static/js/2.4005359f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5ad8c5cbe0b8bcfa085",
+    "url": "/meetup/static/js/main.2bed320d.chunk.js"
+  },
+  {
+    "revision": "5b15bedc16c52474edcc",
+    "url": "/meetup/static/js/runtime-main.61f01ad6.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/meetup/precache-manifest.dfa859b46c620b31c4a48120f7bdcf5f.js"
+  "/meetup/precache-manifest.d02ba00ecaaafb4d90d97550c1e6e498.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/meetup/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/meetup/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
